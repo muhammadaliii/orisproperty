1a78200c5b3049d890772228ec6307c1a5837bb9
Muhammad Ali <muhammadali.alaydrus@icloud.com>
2021-12-26 22:28:51 +0700

    update layout & add autocomplete

---
+++ src/js/components/autocomplete.js
@@ -0,0 +1,169 @@
+// function propertySearch() {
+// 	var options = {
+		
+// 		// url: "resources/heroes.json",
+
+// 		data: {
+// 			"city": [
+// 				{"cityName": "Jakarta", "provinceName": "DKI Jakarta"},
+// 				{"cityName": "Bandung", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Surabaya", "provinceName": "Jawa Timur"},
+// 				{"cityName": "Bekasi", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Semarang", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Denpasar", "provinceName": "Bali"},
+// 				{"cityName": "Tanggerang", "provinceName": "Banten"},
+// 				{"cityName": "Jogja", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Cianjur", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Palembang", "provinceName": "Sumatera Selatan"},
+// 			],
+// 		},
+
+// 		categories: [
+// 			{
+// 				listLocation: "city",
+// 				maxNumberOfElements: 10,
+// 				// header: "Marvel - heroes"
+// 			},
+// 		],
+
+// 		getValue: function(element) {
+// 			return element.cityName;
+// 		},
+
+// 		template: {
+// 			type: "description",
+// 			fields: {
+// 				description: "provinceName"
+// 			}
+// 		},
+
+// 		list: {
+// 			maxNumberOfElements: 12,
+// 			match: {
+// 				enabled: true
+// 			},
+// 			sort: {
+// 				enabled: true
+// 			}
+// 		},
+
+// 	};
+
+// 	$('#property-search').easyAutocomplete(options);
+// }
+
+// function homeSearch() {
+// 	var options = {
+		
+// 		// url: "resources/heroes.json",
+
+// 		data: {
+// 			"city": [
+// 				{"cityName": "Jakarta", "provinceName": "DKI Jakarta"},
+// 				{"cityName": "Bandung", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Surabaya", "provinceName": "Jawa Timur"},
+// 				{"cityName": "Bekasi", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Semarang", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Denpasar", "provinceName": "Bali"},
+// 				{"cityName": "Tanggerang", "provinceName": "Banten"},
+// 				{"cityName": "Jogja", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Cianjur", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Palembang", "provinceName": "Sumatera Selatan"},
+// 			],
+// 		},
+
+// 		categories: [
+// 			{
+// 				listLocation: "city",
+// 				maxNumberOfElements: 10,
+// 				// header: "Marvel - heroes"
+// 			},
+// 		],
+
+// 		getValue: function(element) {
+// 			return element.cityName;
+// 		},
+
+// 		template: {
+// 			type: "description",
+// 			fields: {
+// 				description: "provinceName"
+// 			}
+// 		},
+
+// 		list: {
+// 			maxNumberOfElements: 12,
+// 			match: {
+// 				enabled: true
+// 			},
+// 			sort: {
+// 				enabled: true
+// 			}
+// 		},
+
+// 	};
+
+// 	$('#home-search').easyAutocomplete(options);
+// }
+
+// function globalSearch() {
+// 	var options = {
+
+// 		// url: "resources/heroes.json",
+		
+// 		data: {
+// 			"city": [
+// 				{"cityName": "Jakarta", "provinceName": "DKI Jakarta"},
+// 				{"cityName": "Bandung", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Surabaya", "provinceName": "Jawa Timur"},
+// 				{"cityName": "Bekasi", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Semarang", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Denpasar", "provinceName": "Bali"},
+// 				{"cityName": "Tanggerang", "provinceName": "Banten"},
+// 				{"cityName": "Jogja", "provinceName": "Jawa Tengah"},
+// 				{"cityName": "Cianjur", "provinceName": "Jawa Barat"},
+// 				{"cityName": "Palembang", "provinceName": "Sumatera Selatan"},
+// 			],
+// 		},
+
+// 		categories: [
+// 			{
+// 				listLocation: "city",
+// 				maxNumberOfElements: 10,
+// 				// header: "Marvel - heroes"
+// 			},
+// 		],
+
+// 		getValue: function(element) {
+// 			return element.cityName;
+// 		},
+
+// 		template: {
+// 			type: "description",
+// 			fields: {
+// 				description: "provinceName"
+// 			}
+// 		},
+
+// 		list: {
+// 			maxNumberOfElements: 12,
+// 			match: {
+// 				enabled: true
+// 			},
+// 			sort: {
+// 				enabled: true
+// 			}
+// 		},
+
+// 	};
+
+// 	$('#global-search').easyAutocomplete(options);
+// }
+
+(function($){
+
+	// propertySearch();
+	// homeSearch();
+	// globalSearch();
+
+})(jQuery);
